647f95a911abe55f04c8505622e3d1c6f4a4a6fc
Luan Valente <luanbvalente@hotmail.com>
2024-06-27 14:29:34 -0300

    Criação de conta

--- index.js
+++ index.js
@@ -22,11 +22,21 @@ function operation() {
     }])
     .then(answer => {
         const action = answer['action']
-        console.log(action)
+        
+        if (action === 'Criar Conta') {
+            createAccount()
+        }
+
     })
     .catch(err => console.log(err))
 }
 
+// Criando conta
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
 
 
 
